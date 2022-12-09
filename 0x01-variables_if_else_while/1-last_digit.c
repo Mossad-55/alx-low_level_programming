c0f364f06fcceb87478465096e0eb4fa88414b6b
Mossad-55 <mosad55522@gmail.com>
2022-12-09 16:58:41 +0200

    Fixed 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,8 +14,9 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_dig = n % 10;
+	int last_dig;
 
+	last_dig = n % 10;
 	if (last_dig > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_dig);
 	else if (last_dig < 6 && last_dig != 0)
