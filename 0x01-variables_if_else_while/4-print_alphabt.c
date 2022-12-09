c0628f55ba7dd9c26c83f8a873e6a20ac305cfca
Mossad-55 <mosad55522@gmail.com>
2022-12-09 17:32:07 +0200

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Prints all the alphabets in lowercase except q-e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c == 'q' || c == 'e')
+			continue;
+		putchar(c++);
+	}
+	putchar('\n');
+
+	return (0);
+}
