aea3adee407ee0895b16a75aa21f25e3aca29f06
Mossad-55 <mosad55522@gmail.com>
2022-12-09 18:53:28 +0200

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Prints all numbers followed by ,
+ * Allowed function: putchar()
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		putchar(i++);
+		if (i == 57)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
