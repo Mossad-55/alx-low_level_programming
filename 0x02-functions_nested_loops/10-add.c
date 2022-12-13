5da3b379f9c07c6d2190da2dfe198490f1b16b56
Mossad-55 <mosad55522@gmail.com>
2022-12-13 16:20:31 +0200

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add -  Returens the sum of two numbers.
+ * @x: a param of type int. (first number)
+ * @y: a param of type int. (second number)
+ * Return: int.
+ */
+int add(int x, int y);
+{
+	return (x + y);
+}
