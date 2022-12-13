da80d444149d4d171a4b4aff480d5b6503f2a88f
Mossad-55 <mosad55522@gmail.com>
2022-12-13 17:15:44 +0200

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,13 +10,15 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (n = n; n <= 98; n++)
+		for (n = n; n < 98; n++)
 			printf("%d, ", n);
+		printf("%d", 98);
 	}
 	else if (n > 98)
 	{
 		for (n = n; n >= 98; n--)
 			printf("%d, ", n);
+		printf("%d", 98);
 	}
 	else
 		printf("%d", 98);
