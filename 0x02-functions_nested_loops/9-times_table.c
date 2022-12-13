de0e65f3480fa413c40e90b19a9fa78125e68b2f
Mossad-55 <mosad55522@gmail.com>
2022-12-13 16:11:52 +0200

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,21 +6,28 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, res;
 
 	for (i = 0; i < 9; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar((i * j) + '0');
-			_putchar(',');
-			if (i * j < 10)
+			res = i * j;
+			if (j != 0)
 			{
-				_putchar(' ');
+				_putchar(',');
 				_putchar(' ');
 			}
-			else
+			if (res >= 10)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else if (res < 10)
+			{
 				_putchar(' ');
+				_putchar((res % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
