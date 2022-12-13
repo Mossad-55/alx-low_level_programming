88eaf1558a0d5951fdc01c899918e10eb80879ae
Mossad-55 <mosad55522@gmail.com>
2022-12-13 15:14:02 +0200

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isLower -  returns 1 if it's lower otherwise 0.
+ * @c: character as a param.
+ * Return: int.
+ */
+int _isLower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+
+return (0);
+}
