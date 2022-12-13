d69f9e5522fc930e7b9bc157c65da9bb14482b12
Mossad-55 <mosad55522@gmail.com>
2022-12-13 20:20:11 +0200

    Task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the times table of a given number.
+ * @n: is a param of type int.
+ * Return: void.
+ */
+void print_times_table(int n)
+{
+	int i, j, res;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			res = i * j;
+			if (j != 0)
+				printf(", ");
+			if (res < 10)
+				printf("  %d", res);
+			else if (res >= 10 && res < 100)
+				printf(" %d", res);
+			else
+				printf("%d", res);
+		}
+		printf("\n");
+	}
+}
