87555664b584e979bb36303ce14e2c3c7ee1f1c4
Mossad-55 <mosad55522@gmail.com>
2022-12-13 14:33:02 +0200

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Printing the word "_putchar" followed by a new line
+ * Using only _putchar() from 'unistd.h'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char* srt;
+	int i;
+
+	str = "_putchar";
+	for (i = 0; i < 8; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+
+	return (0);
+}
