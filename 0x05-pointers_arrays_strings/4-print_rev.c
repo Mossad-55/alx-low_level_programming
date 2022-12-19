9de0a41dc2e8255c56db4effa56f8c28d02bd42c
Mossad-55 <mosad55522@gmail.com>
2022-12-19 17:28:46 +0200

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse a string.
+ * @s: pointer points to a str.
+ * Return: void.
+ */
+void print_rev(char *s)
+{
+	int i, j, temp;
+
+	j = 0;
+	while (s[j] != '\0')
+		j++;
+	j--;
+	for (i = 0; i < j; i++)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j--] = temp;
+	}
+}
