a33f57c075644a8b68b0649d0cd1d7f9536675a7
Mossad-55 <mosad55522@gmail.com>
2022-12-21 19:16:47 +0200

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *cap_string - captialize all words of a string.
+ * @s : pointer points to an array of chars.
+ * Return: string (char array).
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 32;
+
+	i = 1;
+	while (s[i] != '\0')
+	{
+		if (s[i - 1] == ' ')
+			s[i] -= 32;
+		i++;
+	}
+
+	return (s);
+}
