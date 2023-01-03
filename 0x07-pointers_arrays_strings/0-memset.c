f90fbad716235f9dffbeea8a9c39f97c4f0b0c33
Mossad-55 <mosad55522@gmail.com>
2023-01-03 14:24:51 +0200

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a contant byte.
+ * @s: pointer (first param).
+ * @b: char (second param).
+ * @n: int (third param).
+ * Return: a pointer.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+
+	return (s);
+}
