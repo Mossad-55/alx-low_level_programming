cec70a8217c828678842472fc61e27e3388472ed
Mossad-55 <mosad55522@gmail.com>
2023-01-03 16:11:11 +0200

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,8 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * *_strstr - located a substring.
+ * *_strstr - locates a substring.
  * @haystack: pointer (first param).
  * @needle: pointer (second param).
  * Return: pointer.
