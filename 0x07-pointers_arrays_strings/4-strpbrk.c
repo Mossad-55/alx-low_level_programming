3aac1c8200b99a942d295bccb6098e21192bfc32
Mossad-55 <mosad55522@gmail.com>
2023-01-03 15:44:49 +0200

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - search a string for any of a set of bytes.
+ * @s: pointer (first param).
+ * @accept: pointer (second param).
+ * Return: pointer.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *temp;
+
+	temp = accept;
+	while (*s++)
+	{
+		while (*accept++)
+		{
+			if (*(s - 1) == *(accept - 1))
+				return (s);
+		}
+		accept = temp;
+	}
+	return (NULL);
+}
