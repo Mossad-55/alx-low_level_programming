fcf333a225881d30ce597cc72cb094f2a2cee759
Mossad-55 <mosad55522@gmail.com>
2023-01-04 22:53:45 +0200

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to power y.
+ * @x: int (first param).
+ * @y: int (second param).
+ * Return: int.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (x);
+
+	return (x * _pow_recursion(y - 1));
+}
