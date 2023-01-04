6902e777dfb6e47755a4224e978338819459d560
Mossad-55 <mosad55522@gmail.com>
2023-01-04 22:31:50 +0200

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (s[0] == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar(*(s + 0));
-	return _puts_recursion(*s + 1);
+	_putchar(s[0]);
+	return _puts_recursion(s + 1);
 }
