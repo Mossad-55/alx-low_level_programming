fd0c0fb63a77e554b11e6e2b65fb5023f1dc4977
Mossad-55 <mosad55522@gmail.com>
2023-01-11 01:08:32 +0200

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - copies a string into a new allocated memory.
+ * @str: string.
+ *
+ * Return: pointer or null.
+ */
+char *_strdup(char *str)
+{
+	char *new_str;
+	unsigned int i, len;
+
+	i = len = 0;
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	new_str = malloc(sizeof(char) * (len + 1));
+	if (new_str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+		new_str[i] = str[i++];
+	new_str[i] = '\0';
+	return (new_str);
+}
