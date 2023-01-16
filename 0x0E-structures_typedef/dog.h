78b2e4d62bc4ee9566d20fc52ecc6c5c58b34fc7
Mossad-55 <mosad55522@gmail.com>
2023-01-17 00:52:46 +0200

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,12 @@
  *
  * Description: this is a struct for a dog.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
