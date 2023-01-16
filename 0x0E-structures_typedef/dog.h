80d077bcb0e8e3e30026c4681eb0f02e6d1b4727
Mossad-55 <mosad55522@gmail.com>
2023-01-17 00:35:40 +0200

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct.
+ * @name: First member.
+ * @age: Second member.
+ * @owner: Third member.
+ *
+ * Description: this is a struct for a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
