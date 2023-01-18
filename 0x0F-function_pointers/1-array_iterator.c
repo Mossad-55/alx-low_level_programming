de1f0fb9f3904d0e7d2dc700c66d8b23076dc26a
Mossad-55 <mosad55522@gmail.com>
2023-01-18 13:34:11 +0200

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,15 +9,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
 	if (array == NULL || action == NULL)
 		return;
 
 	i = 0;
 	while (i < size)
-	{
-		action(array[i]);
-		i++;
-	}
+		action(array[i++]);
 }
