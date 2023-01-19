e47eebb417dc9b27d690a01d861012ce3356bb82
Mossad-55 <mosad55522@gmail.com>
2023-01-20 00:30:28 +0200

    Task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of numbers.
+ * @n: number of arguments.
+ *
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list nums;
+
+	va_start(nums, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(nums, int);
+
+	va_end(nums);
+	return (sum);
+}
