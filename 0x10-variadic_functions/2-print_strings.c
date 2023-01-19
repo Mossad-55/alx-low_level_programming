193b111f9b21e41bc111dead07b246da422c0f32
Mossad-55 <mosad55522@gmail.com>
2023-01-20 00:49:10 +0200

    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else if (separator && i == 0)
 			printf("%s", s);
 		else
-			printf("%s%s", s);
+			printf("%s%s", separator, s);
 	}
 	printf("\n");
 
