5b262ef1fa4252b74b8006f6aed4247d3455c0a1
Mossad-55 <mosad55522@gmail.com>
2023-01-20 00:21:59 +0200

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,14 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum = 0;
-	va_list args;
+	va_list nums;
 
-	va_start(args, n);
+	va_start(nums, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, int);
+		sum += va_arg(nums, int);
 
-	va_end(args);
+	va_end(nums);
 
 	return (sum);
 }
