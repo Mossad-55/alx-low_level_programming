11edf38dae17d9edb082f797d421b838b9f1e8bc
Mossad-55 <mosad55522@gmail.com>
2023-02-01 01:14:07 +0200

    Task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * *reverse_listint - reverses a linked list.
+ * @head: linked list.
+ *
+ * Return: linked list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next = NULL;
+	listint_t *prev = NULL;
+
+	while (*head)
+	{
+		*next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = rev;
+	return (*head);
+}
