da48829b976c9caeb5b0cc9848c48eebd375f1c8
Mossad-55 <mosad55522@gmail.com>
2023-01-31 15:57:53 +0200

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,15 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t n;
+	size_t len = 0;
 
-	n = 0;
 	while (h)
 	{
 		printf("%d\n", h->n);
-		n++;
+		len++;
 		h = h->next;
 	}
 
-	return (n);
+	return (len);
 }
